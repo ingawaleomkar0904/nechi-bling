4ae359c5b51ba16e47e47dd5900271c6fc704e87
ingawaleomkar <ingawaleomkar9@gmail.com>
2026-06-10 13:48:47 +0530

    Configure React Router basename using Vite BASE_URL

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Navbar />
       <Routes>
